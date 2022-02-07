7f83aa90e9ae5b7f0d5d338b92f91199483d82d5
jhajdari <jhajdari@ritech.co>
2022-02-07 15:19:04 +0100

    Add Dark-mode-app

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const articles = [
+  {
+    id: 1,
+    title: "the Perfect Codbase",
+    date: new Date(2021, 10, 11),
+    length: 10,
+    snippet: `Watch and learn. Then try it`,
+  },
+  {
+    id: 2,
+    title: "goodby, hello bugs",
+    date: new Date(2022, 10, 22),
+    length: 5,
+    snippet: `Dont let bugs take advantage`,
+  },
+  {
+    id: 3,
+    title: "My top 10 code editors",
+    date: new Date(2021, 7, 11),
+    length: 5,
+    snippet: `A personal reflection.`,
+  },
+];
+
+export default articles;
